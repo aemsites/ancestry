273d9b674e256d963993c211fe8b6acd59909df2
Darin Kuntze <dkuntze@gmail.com>
2024-09-24 16:49:19 -0500

    wip

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,14 @@
-import { getMetadata } from '../../scripts/aem.js';
+import { getMetadata, loadCSS, loadScript } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
+
+function fixFooter(oldFooter) {
+  const ancFooter = oldFooter.querySelector('#footer');
+  if (!ancFooter) {
+    oldFooter.setAttribute('data-block-status', 'loaded');
+  } else {
+    ancFooter.setAttribute('data-block-status', 'loaded');
+  }
+}
 
 /**
  * loads and decorates the footer
@@ -7,14 +16,29 @@ import { loadFragment } from '../fragment/fragment.js';
  */
 export default async function decorate(block) {
   // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-  const fragment = await loadFragment(footerPath);
+  // const footerMeta = getMetadata('footer');
+  // const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  // const fragment = await loadFragment(footerPath);
+
+  const ancResp = await fetch('https://navigation.ancestry.com/footer/0/3.0/standard/en-us/false', {
+    mode: 'cors',
+  });
+
+  if (!ancResp.ok) {
+    console.error('Failed to load Ancestry footer');
+  }
+
+  const ancJson = await ancResp.json();
 
   // decorate footer DOM
   block.textContent = '';
   const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  footer.id = 'FooterRegion';
+  footer.innerHTML = ancJson[0].Views[0].Content;
+  // while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  fixFooter(footer.querySelector('ui-custom'));
 
   block.append(footer);
+  await loadCSS(ancJson[0].Resources[0].Src);
+  await loadScript('../../scripts/footer-f2f29243.js', { type: 'text/javascript' });
 }
