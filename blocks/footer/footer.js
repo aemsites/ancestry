87311881e58c646d929716b243bb9e015b4e19d0
Ujjwal Gupta <ujjwal5@users.noreply.github.com>
2024-09-25 21:59:56 +0530

    Decorating footer block (#26)
    * Decorating footer block
    
    * Fix lint issues

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,6 +1,80 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
+function createOptGroup(select, mainItem, subItems) {
+  const groupLabel = mainItem.firstChild.textContent.trim();
+  const optGroup = document.createElement('optgroup');
+  optGroup.label = groupLabel;
+
+  subItems.forEach((link) => {
+    const opt = document.createElement('option');
+    opt.value = link.href;
+    opt.textContent = link.textContent;
+    optGroup.appendChild(opt);
+  });
+
+  select.appendChild(optGroup);
+}
+
+function appendOptions(select, subItems) {
+  subItems.forEach((link) => {
+    const opt = document.createElement('option');
+    opt.value = link.href;
+    opt.textContent = link.textContent;
+    select.appendChild(opt);
+  });
+}
+
+function wrapAndReplace(listWrapper, select, className, isHideLanguageDropdown) {
+  const div = document.createElement('div');
+  div.classList.add(`${className}-div`);
+  div.appendChild(select);
+  div.insertBefore(listWrapper.previousElementSibling, select);
+
+  listWrapper.replaceWith(div);
+
+  if (className === 'select-language' && isHideLanguageDropdown) {
+    div.style.display = 'none';
+  }
+}
+
+function addChangeEvent(select) {
+  select.addEventListener('change', (event) => {
+    if (event.target.value) {
+      window.location.href = event.target.value;
+    }
+  });
+}
+
+function convertListToDropdown(listWrappers, isHideLanguageDropdown) {
+  listWrappers.forEach((listWrapper) => {
+    const select = document.createElement('select');
+    const mainItems = listWrapper.querySelectorAll(':scope > li');
+    let className;
+
+    mainItems.forEach((mainItem) => {
+      const subList = mainItem.querySelector('ul');
+      const subItems = subList ? subList.querySelectorAll('li a') : mainItem.querySelectorAll('a');
+
+      if (subItems.length > 0) {
+        if (subList) {
+          className = 'select-other-sites';
+          createOptGroup(select, mainItem, subItems);
+        } else {
+          className = 'select-language';
+          appendOptions(select, subItems);
+        }
+      }
+    });
+
+    if (className) {
+      select.classList.add(className);
+      wrapAndReplace(listWrapper, select, className, isHideLanguageDropdown);
+      addChangeEvent(select);
+    }
+  });
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
@@ -17,4 +91,7 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
+
+  const isHideLanguageDropdown = block.querySelector('.footer-select').dataset.hideLanguage === 'true';
+  convertListToDropdown(block.querySelectorAll('.footer-select > div > ul'), isHideLanguageDropdown);
 }
