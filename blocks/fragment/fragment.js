53a2effb978e987b983ff6b0701325e2884d64e9
ujjgupta <ujjgupta@adobe.com>
2024-11-19 11:07:28 +0530

    Revert "Bug fix: fragment block should include fragment classnames"
    This reverts commit cb18410235b22fea0d978e620ace62c92240da16.

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -49,9 +49,7 @@ export default async function decorate(block) {
     const fragmentSection = fragment.querySelector(':scope .section');
     if (fragmentSection) {
       block.closest('.section').classList.add(...fragmentSection.classList);
-      const clonedBlockDiv = block.closest('.fragment').cloneNode(false);
-      clonedBlockDiv.append(...fragmentSection.childNodes);
-      block.closest('.fragment').replaceWith(clonedBlockDiv);
+      block.closest('.fragment').replaceWith(...fragment.childNodes);
     }
   }
 }
